318b9e7671d64082daf0f8c98d6b98764a4259e0
김동영 <dongyi@gimdong-yeong-ui-MacBookAir.local>
2025-11-18 13:39:54 +0900

    feat: 금 가격 데이터 s3 로드 구현 #14

--- dags/gold_price_etl.py
+++ dags/gold_price_etl.py
@@ -4,47 +4,47 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
 import yfinance as yf
 import pandas as pd
-import boto3
 import io
 
 BUCKET_NAME = "economic-data-storage"
-S3_KEY_RAW = "raw-data/exchange_rate.csv"
+S3_KEY_RAW_GOLD = "raw-data/gold_price.csv"
 
-def fetch_krw_usd_rate():
-    # KRW/USD 환율 가져오기
-    df = yf.download("KRW=X", period="10d", interval="1h")
-    
+def fetch_gold_price():
+    # 금 가격 가져오기
+    df = yf.download("GC=F", period="5d", interval="1h")
+
     csv_buffer = io.StringIO()
     df.to_csv(csv_buffer)
     return csv_buffer.getvalue()
 
-def upload_to_s3(csv_data):
+def upload_to_s3(csv_data: str, s3_key: str):
 
     hook = S3Hook(aws_conn_id="my_s3")
     hook.load_string(
         string_data=csv_data,
-        key=S3_KEY_RAW,
+        key=s3_key,
         bucket_name=BUCKET_NAME,
         replace=True,
     )
 
 with DAG(
-    dag_id="exchange_rate_etl",
+    dag_id="gold_price_etl",
     start_date=datetime(2025, 1, 1),
     schedule_interval=None,   # 스케줄 고려 X → 수동 실행용
     # schedule_interval="*/10 * * * *",
     catchup=False,
-) as dag:
+) as dag_gold:
 
-    fetch_task = PythonOperator(
-        task_id="fetch_exchange_rate",
-        python_callable=fetch_krw_usd_rate,
+    fetch_gold = PythonOperator(
+        task_id="fetch_gold_price",
+        python_callable=fetch_gold_price,
     )
 
-    upload_task = PythonOperator(
+    upload_gold = PythonOperator(
         task_id="upload_to_s3",
         python_callable=upload_to_s3,
-        op_args=[fetch_task.output],     # CSV 결과 전달
+        op_args=[fetch_gold.output],     # CSV 결과 전달
+        op_kwargs={"s3_key": S3_KEY_RAW_GOLD}
     )
 
-    fetch_task >> upload_task
+    fetch_gold >> upload_gold
